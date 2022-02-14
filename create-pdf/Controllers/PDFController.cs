6b8b8a2d4d46c75978e7f35fdf507009ff17b941
micaela <micaela.mqs@gmail.com>
2022-02-14 20:55:20 -0300

    basic pdf file structure

--- create-pdf/Controllers/PDFController.cs
+++ create-pdf/Controllers/PDFController.cs
@@ -1,6 +1,33 @@
-﻿namespace create_pdf.Controllers
+﻿using create_pdf.Entities;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace create_pdf.Controllers
 {
-    public class PDFController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PDFController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<Person> CreatePdf()
+        {
+            Document pdf = new(PageSize.A4);
+            MemoryStream stream = new();
+            PdfWriter.GetInstance(pdf, stream);
+
+            pdf.Open();
+
+            var title = new Paragraph("Test!");
+
+            pdf.Add(title);
+
+            pdf.Close();
+
+            byte[] content = stream.ToArray();
+
+            return File(content, "application/pdf", "teste.pdf");
+        }
     }
 }
--- create-pdf/Entities/Person.cs
+++ create-pdf/Entities/Person.cs
@@ -5,5 +5,5 @@
         public int Id { get; set; } 
         public string Name { get; set; }
         public int Age { get; set; }
-    }
+    }    
 }
