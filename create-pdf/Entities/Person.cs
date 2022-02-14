47261529539fbdd1fad19927f64a7ae403272a20
micaela <micaela.mqs@gmail.com>
2022-02-14 19:22:57 -0300

    person entity

---
+++ create-pdf/Entities/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace create_pdf.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
